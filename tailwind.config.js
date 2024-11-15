78929f808a318e531b59363f34b099e6a1d590e3
kazutakanakamura711 <a711st777@gmail.com>
2024-11-15 19:03:37 +0900

    Add scroll

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'custom-blue': '#106ACC',
+        'dn-layer-1': '#EDEDED',
+        'dn-layer-2': '#F7F7F7F7',
+        'dn-layer-3': '#FFFFFF',
+        'dn-layer-1-dark': '#0F0F0F',
+        'dn-layer-2-dark': '#272727',
+        'dn-layer-3-dark': '#434343',
+        'dn-primary-1': '#106ACC',
+        'dn-primary-2': '#0686C9',
+        'dn-primary-3': '#032A58',
+        'dn-outline-border': '#BFBFBF',
       },
     },
   },
