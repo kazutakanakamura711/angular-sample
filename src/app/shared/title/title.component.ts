a4fafcd516a98bb3c754460d733a638660888de7
kazutakanakamura711 <a711st777@gmail.com>
2024-11-27 22:13:21 +0900

    Move concurrent-edit to the title component

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { EditingUser } from '../concurrent-edit/concurrent-edit.component';
+import {
+  ConcurrentEditComponent,
+  EditingUser,
+} from '../concurrent-edit/concurrent-edit.component';
 
 @Component({
   selector: 'app-title',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ConcurrentEditComponent],
   templateUrl: './title.component.html',
   styleUrl: './title.component.scss',
 })
