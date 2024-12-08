05ac931765b405d2dbab678a8a63a9226cdb73a6
kazutakanakamura711 <a711st777@gmail.com>
2024-12-08 21:43:08 +0900

    Fix pull down component

--- src/app/shared/pull-down/pull-down.component.ts
+++ src/app/shared/pull-down/pull-down.component.ts
@@ -1,11 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  HostListener,
+} from '@angular/core';
 import { CheckboxComponent } from '../checkbox/checkbox.component';
 
 export interface ListItem {
   id: string;
   label: string;
 }
+
+export interface Disclosure {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+// 型ガード
+const isHTMLElement = (target: EventTarget | null): target is HTMLElement => {
+  return target instanceof HTMLElement;
+};
+
 @Component({
   selector: 'pull-down',
   standalone: true,
@@ -14,6 +32,8 @@ export interface ListItem {
   styleUrl: './pull-down.component.scss',
 })
 export class PullDownComponent {
+  // プルダウンが開いているかどうかを管理
+  @Input() isOpen = false;
   // セレクトボックスのwidth
   @Input() width?: string;
   // マルチセレクトにするかどうか
@@ -28,15 +48,19 @@ export class PullDownComponent {
   @Input() selectedItems: ListItem[] = [];
   // 項目がクリックされたときに親コンポーネントに通知するイベント
   @Output() itemSelected = new EventEmitter<ListItem[]>();
-
-  // ドロップダウンが開いているかどうかを管理
-  isOpen = false;
+  // 親コンポーネントに閉じるイベントを通知
+  @Output() onClose = new EventEmitter<void>();
 
   // デフォルトのメッセージ
   private defaultPlaceholderMessage = '選択してください';
 
-  // 前回のisOpenの状態を保存する変数
-  private prevIsOpen = false;
+  // 画面全体のクリックを監視
+  @HostListener('document:click', ['$event'])
+  onOutsideClick(event: MouseEvent): void {
+    if (!isHTMLElement(event.target) || event.target.closest('.pull-down'))
+      return;
+    this.onClose.emit();
+  }
 
   // 選択された項目をカンマ区切りで返すgetter
   get selectedItemsString(): string {
@@ -45,12 +69,12 @@ export class PullDownComponent {
       : this.defaultPlaceholderMessage;
   }
 
-  // 項目を選択したときに呼ばれるメソッド
+  // 項目を選択したときの処理
   selectItem(item: ListItem) {
     // シングルセレクトの場合
     if (!this.isMulch) {
-      this.selectedItems = [item]; // 選択されたアイテムだけを保持
-      this.isOpen = false; // リストを閉じる
+      this.selectedItems = [item];
+      this.onClose.emit();
       return;
     }
 
@@ -76,13 +100,5 @@ export class PullDownComponent {
     return this.selectedItems.some(
       (selectedItem) => selectedItem.id === item.id,
     );
-  }
-
-  // isOpen の変化を監視
-  ngDoCheck() {
-    if (this.prevIsOpen && !this.isOpen) {
-      this.itemSelected.emit(this.selectedItems);
-    }
-    this.prevIsOpen = this.isOpen;
   }
 }
