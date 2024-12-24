820a9f78ade3bb748a149548b1e29bf7947e65bd
kazutakanakamura711 <a711st777@gmail.com>
2024-12-24 19:22:10 +0900

    Fix validation function

--- src/app/utilities/validation/common.validation.ts
+++ src/app/utilities/validation/common.validation.ts
@@ -6,36 +6,43 @@ import {
 } from '@angular/forms';
 
 // 必須チェック
-export const checkRequiredForText = (
+export const checkRequiredForText: ValidatorFn = (
   control: AbstractControl,
 ): ValidationErrors | null => {
-  const value = control.value?.trim() || '';
-  if (!value) {
+  const requiredValidator = Validators.required(control);
+  if (requiredValidator?.['required']) {
     return { required: 'この項目は必須です。' };
   }
   return null;
 };
 
 // 文字数チェック
-export const checkNumChars =
-  (
-    minLength?: number,
-    maxLength?: number,
-  ): ((control: AbstractControl) => ValidationErrors | null) =>
-  (control: AbstractControl): ValidationErrors | null => {
-    const value = control.value || '';
-    if (minLength != null && value.length < minLength) {
+export const checkNumChars = (
+  minLength?: number,
+  maxLength?: number,
+): ValidatorFn => {
+  return (control: AbstractControl): ValidationErrors | null => {
+    if (
+      minLength != null &&
+      Validators.minLength(minLength)(control)?.['minlength']
+    ) {
       return {
         minlength: `この項目は${minLength}文字以上で入力してください。`,
       };
     }
-    if (maxLength != null && value.length > maxLength) {
+
+    if (
+      maxLength != null &&
+      Validators.maxLength(maxLength)(control)?.['maxlength']
+    ) {
       return {
         maxlength: `この項目は${maxLength}文字以下で入力してください。`,
       };
     }
+
     return null;
   };
+};
 
 // メールアドレスチェック
 export const checkEmailFormat: ValidatorFn = (
