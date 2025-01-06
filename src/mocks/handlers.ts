b6224f6bf99c8781b8fb32190839e34a47fd78ac
kazutakanakamura711 <a711st777@gmail.com>
2025-01-06 13:22:53 +0900

    Add isShouldFail flag

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,35 +1,54 @@
 import { http, HttpResponse } from 'msw';
 
+// false: 正常時用, true: エラー時用
+const isShouldFail = false;
+
 export const handlers = [
   http.get('https://api.example.com/users', () => {
-    return HttpResponse.json([
-      {
-        id: 'ABCDE0',
-        name: 'Taro Yamada',
-        permission: {
-          hasReadable: true,
-          hasEditable: true,
-          hasPublishable: true,
+    if (isShouldFail) {
+      return HttpResponse.json(
+        { error: 'Internal Server Error' },
+        {
+          status: 500,
+          statusText: 'Internal Server Error',
         },
-      },
-      {
-        id: 'ABCDE1',
-        name: 'Hanako Matsuyama',
-        permission: {
-          hasReadable: true,
-          hasEditable: true,
-          hasPublishable: false,
+      );
+    }
+
+    return HttpResponse.json(
+      [
+        {
+          id: 'ABCDE0',
+          name: 'Taro Yamada',
+          permission: {
+            hasReadable: true,
+            hasEditable: true,
+            hasPublishable: true,
+          },
         },
-      },
-      {
-        id: 'ABCDE2',
-        name: 'Jiro Ehime',
-        permission: {
-          hasReadable: true,
-          hasEditable: false,
-          hasPublishable: false,
+        {
+          id: 'ABCDE1',
+          name: 'Hanako Matsuyama',
+          permission: {
+            hasReadable: true,
+            hasEditable: true,
+            hasPublishable: false,
+          },
         },
+        {
+          id: 'ABCDE2',
+          name: 'Jiro Ehime',
+          permission: {
+            hasReadable: true,
+            hasEditable: false,
+            hasPublishable: false,
+          },
+        },
+      ],
+      {
+        status: 200,
+        statusText: 'OK',
       },
-    ]);
+    );
   }),
 ];
